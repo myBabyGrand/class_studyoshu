969a70a0058698db2c623df7d24183052507a7a2
mybabygrand <amuro_ray@kakao.com>
2021-04-19 23:29:04 +0900

    first commit

---
+++ src/test/java/com/studyoshu/studyoshu/StudyoshuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.studyoshu.studyoshu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyoshuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
